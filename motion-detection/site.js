a57808a6d8af628a6a290fb32e99846096fce8c5
RitabrataDas343 <dasritabrata5@gmail.com>
2021-12-22 14:43:43 +0530

    [ADD] Motion Detection

---
+++ motion-detection/site.js
@@ -0,0 +1,84 @@
+var $motionBox = $('.motion-box');
+var $glug = $('img');
+
+var scale = 10;	
+var isActivated = false;
+var isTargetInSight = false;
+var isKnockedOver = false;
+var lostTimeout;
+
+function initSuccess() {
+	DiffCamEngine.start();
+}
+
+function initError() {
+	alert('Something went wrong.');
+}
+
+function startComplete() {
+	setTimeout(activate, 500);
+}
+
+function activate() {
+	isActivated = true;
+	play('activated');
+}
+
+function capture(payload) {
+	if (!isActivated || isKnockedOver) {
+		return;
+	}
+
+	var box = payload.motionBox;
+	if (box) {
+		var right = box.x.min * scale + 1;
+		var top = box.y.min * scale + 1;
+		var width = (box.x.max - box.x.min) * scale;
+		var height = (box.y.max - box.y.min) * scale;
+
+		$motionBox.css({
+			display: 'block',
+			right: right,
+			top: top,
+			width: width,
+			height: height
+		});
+
+		if (!isTargetInSight) {
+			isTargetInSight = true;
+			play('i-see-you');
+		} else {
+			play('fire');
+		}
+		clearTimeout(lostTimeout);
+		lostTimeout = setTimeout(declareLost, 2000);
+	}
+}
+
+function knockOver() {
+	isKnockedOver = true;
+	clearTimeout(lostTimeout);
+	$motionBox.hide();
+	play('ow');
+}
+
+
+function declareLost() {
+	isTargetInSight = false;
+	play('target-lost');
+}
+
+function play(audioId) {
+	$('#audio-' + audioId)[0].play();
+}
+
+DiffCamEngine.init({
+	video: document.getElementById('video'),
+	captureIntervalTime: 50,
+	includeMotionBox: true,
+	includeMotionPixels: true,
+	initSuccessCallback: initSuccess,
+	initErrorCallback: initError,
+	startCompleteCallback: startComplete,
+	captureCallback: capture
+});
